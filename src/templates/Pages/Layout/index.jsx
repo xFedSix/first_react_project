8e484ac3bc76d6ad9b222b01956160849712fce7
Siyanka Fiodar <siyankafiodar@gmail.com>
2022-08-23 14:13:47 +0300

    main

--- src/templates/Pages/Layout/index.jsx
+++ src/templates/Pages/Layout/index.jsx
@@ -2,10 +2,7 @@ import React from "react";
 
 import { Outlet } from "react-router-dom";
 import HeaderComponent from "../../../components/HeaderComponent";
-import AboutUs from "../../AboutUs";
-import BunnerComponent from "../../BunnerComponent";
-import OurServices from "../../OurServices";
-import PricingPlan from "../../PricingPlan";
+
 import FooterComponent from "../../../components/FooterComponent/Index";
 
 const Layout = () => {
@@ -13,13 +10,6 @@ const Layout = () => {
     <>
       <>
         <HeaderComponent />
-        {/* <BunnerComponent
-          title="Web development project"
-          subtitle="Very suitable to support all web development projects"
-        />
-        <AboutUs />
-        <OurServices />
-        <PricingPlan /> */}
         <Outlet />
         <FooterComponent />
       </>
