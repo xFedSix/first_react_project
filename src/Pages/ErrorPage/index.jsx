0b92b737672b4204df6a3f4eb8baff288174f09f
Siyanka Fiodar <siyankafiodar@gmail.com>
2022-09-06 20:34:38 +0300

    Updates

--- src/Pages/ErrorPage/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ErrorPage = () => {
-  return <h1>Page not found Please navigate to another pages</h1>;
-};
-
-export default ErrorPage;
--- src/templates/BunnerComponent/constants.js
+++
@@ -1,9 +0,0 @@
-import arrow from "./images/arrow.svg";
-
-export const BANNER_COMPONENT = [
-  {
-    title: "Web development project",
-    subtitle: "Very suitable to support all web development projects",
-    image: arrow
-  },
-];
